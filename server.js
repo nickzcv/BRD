d363873bf69032608c03ad303cdd35b318f175ff
Nick Z <nickzcv@gmail.com>
2017-05-25 22:18:34 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-var express = require('express');
-var port = process.env.PORT || 80;
-var app = express();
+const express = require('express');
+const port = process.env.PORT || 80;
+const app = express();
+const bodyParser = require('body-parser');
+const winston = require('winston');
+// const mongoose = require('mongoose');
 
+app.use(bodyParser.json());
+// root directory from which the static assets are to be served.
 app.use(express.static('./app'));
+// Set up a logger.
+app.locals.logger = new winston.Logger();
+app.locals.logger.add(winston.transports.Console, {
+  colorize: true
+});
+// Log every request.
+app.use(function (req, res, next) {
+  req.app.locals.logger.info('[' + req.method + ']', req.url);
+  next();
+});
+
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/app/index.html');
+});
+
 
 app.listen(port);
 console.log('Server started on port: ' + port);
