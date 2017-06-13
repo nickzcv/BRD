7a2ba7be8a6aad6d2b75cb09969e9603bb9b28c3
Nick Z <nickzcv@gmail.com>
2017-06-13 07:03:36 +0300

    update helper

--- app/js/helpers.js
+++ app/js/helpers.js
@@ -8,3 +8,60 @@ Handlebars.registerHelper('if_eq', function(a, b, opts) {
   else
     return opts.inverse(this);
 });
+
+
+/**
+ * Compare two values
+ *
+ * @param {Number} lvalue Left value to compare
+ * @param {String} operator Operator to apply while comparing
+ * @param {Number} rvalue Right value to compare
+ * @param {Object} options
+ * @property {Function} options.fn
+ * @property {Function} options.inverse
+ *
+ * @todo Clarify parameters' types and descriptions.
+ * @function external:HandlebarsHelper#compare
+ */
+/* jshint ignore:start */
+/* eslint-disable */
+Handlebars.registerHelper('compare', function(lvalue, operator, rvalue, options) {
+  var operators, result;
+  if (arguments.length < 3) {
+    throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+  }
+  if (options === undefined) {
+    options = rvalue;
+    rvalue = operator;
+    operator = "===";
+  }
+  operators = {
+    '==': function(l, r) {
+      return l == r; },
+    '===': function(l, r) {
+      return l === r; },
+    '!=': function(l, r) {
+      return l != r; },
+    '!==': function(l, r) {
+      return l !== r; },
+    '<': function(l, r) {
+      return l < r; },
+    '>': function(l, r) {
+      return l > r; },
+    '<=': function(l, r) {
+      return l <= r; },
+    '>=': function(l, r) {
+      return l >= r; },
+    'typeof': function(l, r) {
+      return typeof l == r; }
+  };
+  if (!operators[operator]) {
+    throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
+  }
+  result = operators[operator](lvalue, rvalue);
+  if (result) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
