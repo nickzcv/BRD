df51b88f4bdb6062d3aea7d7cb43361343d218ec
Nick Z <nickzcv@gmail.com>
2017-06-17 12:13:14 +0300

    add jwt in user schema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
-var User = new Schema({
+var userSchema = new mongoose.Schema({
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  hash: String,
-  salt: String,
+  hash: String, // The hash is created by combining the password provided by the user and the salt, and then applying one-way encryption.
+  salt: String, // The salt is a string of characters unique to each user.
   name: String,
   phone: String,
   location: String,
@@ -17,4 +18,26 @@ var User = new Schema({
   isActive: {type: Boolean, default: false}
 });
 
-module.exports = mongoose.model('User', User);
+userSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
+
+module.exports = mongoose.model('User', userSchema);
