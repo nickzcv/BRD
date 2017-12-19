65b92f4b84acb6d260e694db8e51112502a1f472
Nick Z <nickzcv@gmail.com>
2017-12-19 21:40:41 +0300

    no message

--- routes/upload.js
+++ routes/upload.js
@@ -6,8 +6,8 @@ const path = require('path');
 router.route('/upload/profile')
 	.post(function(req, res) {
     // Get base64 string from json request
-    let userId = req.body.user;
-    let base64Data = req.body.image.replace(/^data:image\/png;base64,/, "");
+    var userId = req.body.user;
+    var base64Data = req.body.image.replace(/^data:image\/png;base64,/, "");
     // Save image
     fs.writeFile('./app/img/avatar/'+userId+'.png', base64Data, 'base64', function(err) {
       if (err) {
