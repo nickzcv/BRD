d887b81ff5ade85e639684351dc972b3e690f34e
Nick Z <nickzcv@gmail.com>
2018-01-07 07:53:45 +0300

    no message

--- app/js/views/home/AdsHomeCollectionView.js
+++ app/js/views/home/AdsHomeCollectionView.js
@@ -13,12 +13,17 @@ app.views.AdsHomeCollectionView = Backbone.Marionette.CollectionView.extend({
       this.collection = new app.collections.AdsHomeCollection();
     }
     this.childViewOptions = {isLoggedIn: brd.controllers.isLoggedIn()};
-    this.emptyView = app.views.SpinnerView;
-    await this.collection.fetch();
-    // If nothing found
-    if (!this.collection.length) {
-      this.emptyView = app.views.EmptyView;
-      this.render();
+    // Start fetching collection data
+    try {
+      await this.collection.fetch();
+      this.emptyView = app.views.SpinnerView;
+      // If nothing found
+      if (!this.collection.length) {
+        this.emptyView = app.views.EmptyView;
+        this.render();
+      }
+    } catch (error) {
+      console.log(error)
     }
   },
 
