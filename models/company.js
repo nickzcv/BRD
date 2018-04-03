390c64883945f54a3cd53a6729b0757e7353b779
Mikalai Zaitsau <mzaitsau@us.ibm.com>
2018-04-03 10:41:52 +0300

    no message

--- models/company.js
+++ models/company.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 var companySchema = new mongoose.Schema({
-
+  name: String,
+  country: {type: Object},
+  city: {type: Object},
+  address: String,
+  phones: {type: []},
+  website: String,
+  logo: String,
+  description: String,
+  year: String,
+  count: String,
+  category: {type: Object},
+  employees: {type: Object},
 });
 
 
