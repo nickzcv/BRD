466d65eb65627d3645ac2040a4f21b3327690226
Mikalai Zaitsau <mzaitsau@us.ibm.com>
2018-07-13 12:59:47 +0300

    no message

--- app/templates.js
+++ app/templates.js
@@ -567,7 +567,7 @@ this["tpl"]["templates"]["main"] = Handlebars.template({"compiler":[7,">= 4.0.0"
 this["tpl"]["templates"]["message"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "  <div class=\"ad-item\">\r\n    <div class=\"ion-ios-plus-outline placeholder add-button\"></div>\r\n    <div class=\"section1\">\r\n      <div class=\"photo\"><span class=\"ion-image\"></span></div>\r\n      <div class=\"content\">\r\n        <div class=\"ad-title\">"
+  return "  <div class=\"ad-item\">\r\n    <div class=\"ion-ios-plus-outline placeholder add-button\"></div>\r\n    <div class=\"section1\">\r\n      <div class=\"photo\"></div>\r\n      <div class=\"content\">\r\n        <div class=\"ad-title\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
     + "</div>\r\n        <div class=\"ad-description\">"
     + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
